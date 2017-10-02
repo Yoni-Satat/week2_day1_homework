f75090e3cd7cf060c5b9d25380c41e664009e799
Yoni Satat <yoni.satat@gmail.com>
2017-10-02 17:21:54 +0100

    added team name test

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -1,15 +1,18 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('../soprts_team.rb')
+require_relative('../sports_team.rb')
 
 class TestSportTeam < MiniTest::Test
   @sport_team
 
-def setup
-  players = ['Michel', 'Jim', 'Sophia']
-  @sport_team = SportTeam.new('Liverpool', players, 'Yoni' )
-end
+  def setup
+    players = ['Michel', 'Jim', 'Sophia']
+    @sport_team = SportTeam.new('Liverpool', players, 'Yoni' )
+  end
 
+  def test_team_name
+    assert_equal('Liverpool', @sport_team.team_name)
+  end
 
 
 
--- sports_team.rb
+++ sports_team.rb
@@ -1,10 +1,14 @@
 class SportTeam
 
   def initialize(team_name, players, coach)
-      @team_name = team_name
-      @players = players
-      @coach = coach
-    end
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def team_name
+    return @team_name
+  end
 
 
 end
